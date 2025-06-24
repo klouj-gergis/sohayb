862f1579055f94c58d51d6816c8fba83fa13520b
klouj gergis <152095106+klouj-gergis@users.noreply.github.com>
2025-06-25 00:28:01 +0300

    completed the why choose us component and waiting for translation

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,7 +6,7 @@ import About from '../common/About.jsx';
 import Products from '../common/Products.jsx';
 import Contact from '../common/Contact.jsx';
 import Footer from '../common/Footer.jsx';
-import WhyChoseUs from '../common/WhyChoseUs.jsx';
+import WhyChoseUs from '../common/WhyChosseUs.jsx';
 
 export default function Home() {
 
