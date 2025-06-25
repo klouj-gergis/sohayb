088ea3c871bbf8859deed6dce5ab9e199d4e5b14
klouj gergis <152095106+klouj-gergis@users.noreply.github.com>
2025-06-25 03:12:52 +0300

    testing bg styling

--- src/components/common/WhyChosseUs.jsx
+++ src/components/common/WhyChosseUs.jsx
@@ -1,14 +1,14 @@
 export default function WhyChooseUs() {
   return (
-    <section className="w-full h-[80vh] max-h-screen overflow-hidden p-6 flex flex-col gap-6">
-      <h3 className="text-3xl text-olive-dark font-semibold text-left md:text-center">
+    <section className="w-full h-[80vh]  max-h-screen overflow-hidden p-6 flex flex-col gap-6">
+      <h3 className="text-3xl text-olive font-semibold text-left md:text-center">
         Why Choose Us
       </h3>
 
       {/* Scrollable Card Container */}
       <div className="flex gap-4 overflow-x-auto scroll-smooth snap-x snap-mandatory h-full pb-4">
         {/* Card 1 */}
-        <div className="snap-start flex-shrink-0 w-72 bg-gray-200 p-5 rounded-md text-olive flex flex-col gap-4">
+        <div className="snap-start flex-shrink-0 w-72 bg-gray-100 p-5 rounded-md text-olive flex flex-col gap-4">
           <h4 className="text-xl font-semibold">1. 100% Pure & Natural</h4>
           <p>
             Free from additives or blends. Pure olive oil straight from our groves in North Sinai, Egypt.
@@ -16,7 +16,7 @@ export default function WhyChooseUs() {
         </div>
 
         {/* Card 2 */}
-        <div className="snap-start flex-shrink-0 w-72 bg-gray-200 rounded-md text-olive flex flex-col gap-4">
+        <div className="snap-start flex-shrink-0 w-72 bg-gray-100 rounded-md text-olive flex flex-col gap-4">
           <img src="/images/natural.jpg" alt="natural" className="rounded-t-md w-full h-40 object-cover" />
           <div className="px-5 pb-5">
             <h4 className="text-xl font-semibold">2. From a Proud Family Farm</h4>
@@ -27,7 +27,7 @@ export default function WhyChooseUs() {
         </div>
 
         {/* Card 3 */}
-        <div className="snap-start flex-shrink-0 w-72 bg-gray-200 p-5 rounded-md text-olive flex flex-col gap-4">
+        <div className="snap-start flex-shrink-0 w-72 bg-gray-100 p-5 rounded-md text-olive flex flex-col gap-4">
           <h4 className="text-xl font-semibold">3. Rich Flavor & Balanced Taste</h4>
           <p>
             Smooth, slightly bitter with a mild peppery finish – perfect for cooking, drizzling, or salad dressings.
@@ -35,7 +35,7 @@ export default function WhyChooseUs() {
         </div>
 
         {/* Card 4 */}
-        <div className="snap-start flex-shrink-0 w-72 bg-gray-200 p-5 rounded-md text-olive flex flex-col gap-4">
+        <div className="snap-start flex-shrink-0 w-72 bg-gray-100 p-5 rounded-md text-olive flex flex-col gap-4">
           <h4 className="text-xl font-semibold">4. Rich in Antioxidants</h4>
           <p>
             Packed with Omega-9, Vitamin E, and polyphenols – great for heart health and immunity.
